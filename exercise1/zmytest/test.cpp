c91ff6018997cb8bf4a0b03e4f1be283b0d3ac52
Luigi Cesaro <luigicesaro2003@libero.it>
2024-04-19 17:19:44 +0200

    StackFactorial test added

--- exercise1/zmytest/test.cpp
+++ exercise1/zmytest/test.cpp
@@ -1,5 +1,5 @@
 
-// #include "..."
+#include "../../stack/stack.hpp"
 
 /* ************************************************************************** */
 
@@ -8,6 +8,29 @@
 using namespace std;
 
 /* ************************************************************************** */
+
+
+void StackFactorial(uint &loctest, uint &testerr, const uint N, uint val, lasd::Stack<int> &s){
+  bool tst;
+  loctest++;
+  std::cout << "Test " << loctest << ": Checking the factorial of " << N << "... ";
+  s.Clear();
+  uint result = 1;
+  uint tstval = N;
+  while(tstval > 0 || !(s.Empty())) {
+     if(tstval > 0) {
+         s.Push(tstval);
+         tstval--;
+      } else {
+         result *= s.Top();
+         s.Pop();
+      }
+  }
+  std::cout << "The factorial of " << N << " is " << result << ": ";
+  tst = (result == val);
+  std::cout << ((tst) ? "Correct" : "Error") << "!" << std::endl;
+  testerr += (1 - (uint)tst);
+}
 
 void mytest() {
   // ...
